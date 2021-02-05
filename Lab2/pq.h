ed5dfeff4bdd3a25b231dc089541d5947741b28a
Trien Ngo <trien.ngo559@gmail.com>
2021-02-05 09:49:17 -0800

    finished header, starting on class defintions

--- Lab2/pq.h
+++ Lab2/pq.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <vector>
+#include <string>
+#include <list>
+
+template <class T>
+class pq
+{
+private:
+	std::vector<T> initial;
+	std::vector<T> goal;
+	std::list<T> solution;
+	void is_solvable();
+	void best_first();
+public:
+	pq() {}; //constructor
+	void read(std::string inputfile);
+	~pq();
+	void write_initial();
+	void write_target();
+	void solve();
+};
